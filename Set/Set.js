7117fe7b5232b2bd4e4c90e7a6ba596049866cfa
loomou <273226981@qq.com>
2020-08-21 22:51:49 +0800

    Set

---
+++ Set/Set.js
@@ -0,0 +1,140 @@
+export default class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  add(element) {
+    if (!this.has(element)) {
+      this.items[element] = element;
+      return true;
+    }
+    return false;
+  }
+
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+      return true;
+    }
+    return false;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  sizeLegacy() {
+    let count = 0;
+    for (let key in this.items) {
+      if (this.items.hasOwnProperty(key)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  values() {
+    return Object.values(this.items);
+  }
+
+  valuesLegacy() {
+    let values = [];
+    for (let key in this.items) {
+      if (this.items.hasOwnProperty(key)) {
+        values.push(key);
+      }
+    }
+    return values;
+  }
+
+  // 集合运算
+  // 并集
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values().forEach(value => unionSet.add(value));
+    otherSet.values().forEach(value => unionSet.add(value));
+    return unionSet;
+  }
+
+  unionPrevious(otherSet) {
+    const unionSet = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      unionSet.add(values[i]);
+    }
+
+    values = otherSet.values();
+    for (let i = 0; i < values.length; i++) {
+      unionSet.add(values[i]);
+    }
+
+    return unionSet;
+  }
+
+  // 交集
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+
+    const values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i])) {
+        intersectionSet.add(values[i]);
+      }
+    }
+    return intersectionSet;
+  }
+
+  intersectionImprove(otherSet) {
+    const intersectionSet = new Set();
+    const values = this.values();
+    const otherValues = otherSet.values();
+    let biggerSet = values;
+    let smallerSet = otherValues;
+    if (otherValues.length - values.length > 0) {
+      biggerSet = otherValues;
+      smallerSet = values;
+    }
+    smallerSet.forEach(value => {
+      if (biggerSet.includes(value)) {
+        intersectionSet.add(value);
+      }
+    });
+    return intersectionSet;
+  }
+
+  // 差集
+  difference(otherSet) {
+    const differenceSet = new Set();
+    this.values().forEach(value => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value);
+      }
+    });
+    return differenceSet;
+  }
+
+  // 自给
+  isSubsetOf(otherSet) {
+    if (this.size() > otherSet.size()) {
+      return false;
+    }
+    let isSubset = true;
+    this.values().every(value => {
+      if (!otherSet.has(value)) {
+        isSubset = false;
+        return false;
+      }
+      return true;
+    });
+    return isSubset;
+  }
+}
---
+++ Set/SetTest.js
@@ -0,0 +1,85 @@
+import Set from "./Set.js";
+
+const set = new Set();
+
+set.add(1);
+console.log(set.values()); // outputs [1]
+console.log(set.has(1)); // outputs true
+console.log(set.size()); // outputs 1
+
+set.add(2);
+console.log(set.values()); // outputs [1, 2]
+console.log(set.has(2)); // true
+console.log(set.size()); // 2
+
+set.delete(1);
+console.log(set.values()); // outputs [2]
+
+set.delete(2);
+console.log(set.values()); // outputs []
+
+// --------- Union ----------
+
+let setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+let setB = new Set();
+setB.add(3);
+setB.add(4);
+setB.add(5);
+setB.add(6);
+
+const unionAB = setA.union(setB);
+console.log(unionAB.values()); // [1, 2, 3, 4, 5, 6]
+
+// --------- Intersection ----------
+
+setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+setB = new Set();
+setB.add(2);
+setB.add(3);
+setB.add(4);
+
+const intersectionAB = setA.intersection(setB);
+console.log(intersectionAB.values()); // [2, 3]
+
+// --------- Difference ----------
+
+setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+setB = new Set();
+setB.add(2);
+setB.add(3);
+setB.add(4);
+
+const differenceAB = setA.difference(setB);
+console.log(differenceAB.values()); // [1]
+
+// --------- Subset ----------
+
+setA = new Set();
+setA.add(1);
+setA.add(2);
+
+setB = new Set();
+setB.add(1);
+setB.add(2);
+setB.add(3);
+
+const setC = new Set();
+setC.add(2);
+setC.add(3);
+setC.add(4);
+
+console.log(setA.isSubsetOf(setB)); // true
+console.log(setA.isSubsetOf(setC)); // false
+
